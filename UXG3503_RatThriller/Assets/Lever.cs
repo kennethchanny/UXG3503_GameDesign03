d88f3a2abc2cd6eebcdaab9c3743a62280630492
Kenneth Chan Kang Er <kennethchanny@gmail.com>
2022-10-20 16:06:22 +0800

    Oi test

---
+++ UXG3503_RatThriller/Assets/Lever.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private Animator animref;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("OI");
+        if(collision.gameObject.tag == "Player")
+        {
+            switch (collision.gameObject.name)
+            {
+
+                case "GretelPlayer1":
+                    {
+                        if(Input.GetKeyDown(KeyCode.DownArrow))
+                        {
+                            ToggleLever();
+                        }
+                    }
+                    break;
+
+                case "HanselPlayer2":
+                    {
+                        if (Input.GetKeyDown(KeyCode.S))
+                        {
+                            ToggleLever();
+                        }
+                    }
+                    break;
+
+
+            }
+
+        }
+    }
+
+    void ToggleLever()
+    {
+        animref.SetTrigger("ToggleLever");
+    }
+    void Start()
+    {
+        animref = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
